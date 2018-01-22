2b4c775219c592a597f643e5cf3a21b53d7062f2
Peter Denno <podenno@gmail.com>
2018-01-22 11:54:08 -0500

    minor deftest change

--- test/pdenno/utils4pmap_test.clj
+++ test/pdenno/utils4pmap_test.clj
@@ -48,3 +48,21 @@
       (is (every? #(== (count %) 10) result)
       (is (every? (fn [t] (every? #(contains? % :timeout) t)) result))))))
 
+
+#_(deftest sleepy-pmap-timeout3-test
+  (testing "that pmap-timeout2 finishes for sleepy processes."
+    (is (= 10
+           (count (repeatedly
+                   10
+                   (fn [] (let [times (pmap-timeout3 sleepy-fn
+                                                     (repeatedly 8 #(+ 10 (* 10 (rand-int 6))))
+                                                     50)]
+                            (apply + (map #(if (number? %) % (:timeout %)) times))))))))))
+
+#_(deftest busy-pmap-timeout3-test
+  (testing "that pmap-timeout3 timesout for busy processes."
+    (let [result (repeatedly 10 #(pmap-timeout3 busy-fn (range 10) 50))]
+      (is (== (count result) 10))
+      (is (every? #(== (count %) 10) result)
+      (is (every? (fn [t] (every? #(contains? % :timeout) t)) result))))))
+
